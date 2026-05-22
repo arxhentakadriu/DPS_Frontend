3043f9dd14784a503d7ca796c6a7de4fd32d8c74
ArxhentëKadriu <149197219+arxhentakadriu@users.noreply.github.com>
2026-05-22 21:32:28 +0200

    Delete src directory

--- src/api/aiApi.js
+++
@@ -1,6 +0,0 @@
-import { http } from './http'
-
-export const aiApi = {
-  chat: (message, history = []) =>
-    http.post('/api/ai/chat', { message, history }),
-}
--- src/api/authApi.js
+++
@@ -1,9 +0,0 @@
-import { http } from './http'
-
-export const authApi = {
-  login: (email, password) => http.post('/api/login', { email, password }),
-
-  register: (payload) => http.post('/api/register', payload),
-
-  getProfile: () => http.get('/api/profile'),
-}
